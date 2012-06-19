6f2000015a441da328e6ad068d6871768a095f63
Jernej Kos <k@jst.sm>
2012-06-19 22:47:55 +0200

    Handle policy routing in default tunnel setup scripts.

--- broker/scripts/bridge_functions.sh
+++ broker/scripts/bridge_functions.sh
@@ -1,3 +1,9 @@
+ensure_policy()
+{
+  ip rule del $*
+  ip rule add $*
+}
+
 ensure_bridge()
 {
   local brname="$1"
@@ -8,6 +14,8 @@ ensure_bridge()
     ip link set dev $brname up
     # TODO The IP address should probably not be hardcoded here?
     ip addr add 10.254.0.2/16 dev $brname
+    # TODO Policy routing should probably not be hardcoded here?
+    ensure_policy from all iif $brname lookup mesh prio 1000
   fi
 }
 
