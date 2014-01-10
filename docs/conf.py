869afb93243f684c61b11937d8521fac89ea6410
Mitar <mitar.git@tnode.com>
2014-01-10 06:42:46 -0800

    Set default theme so that ReadTheDocs use nice theme.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tunneldigger'
-copyright = u'2012, wlan slovenija'
+copyright = u'2012-2013, wlan slovenija'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
