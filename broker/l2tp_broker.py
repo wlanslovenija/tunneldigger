ca790b890e8d0e6b142c8cf118ba91d04fd357d9
Alexander Couzens <lynxis@fe80.eu>
2014-01-19 23:04:01 +0100

    broker: max_tunnels is now a member variable - fix a crash on shutdown

--- broker/l2tp_broker.py
+++ broker/l2tp_broker.py
@@ -693,14 +693,14 @@ class TunnelManager(object):
     """
     logger.info("Setting up the tunnel manager...")
     self.config = config
-    max_tunnels = config.getint('broker', 'max_tunnels')
+    self.max_tunnels = config.getint('broker', 'max_tunnels')
     self.netlink = NetlinkInterface()
     self.conntrack = conntrack.ConnectionManager()
     self.tunnels = {}
     self.cookies = repoze.lru.LRUCache(config.getint('broker', 'max_cookies'))
     self.secret = os.urandom(32)
     id_base = config.getint('broker', 'tunnel_id_base')
-    self.tunnel_ids = range(id_base, id_base + max_tunnels + 1)
+    self.tunnel_ids = range(id_base, id_base + self.max_tunnels + 1)
     self.port_base = config.getint('broker', 'port_base')
     self.interface = config.get('broker', 'interface')
     self.address = config.get('broker', 'address')
@@ -712,7 +712,7 @@ class TunnelManager(object):
     self.setup_hooks()
     
     # Log some configuration variables
-    logger.info("  Maximum number of tunnels: %d" % max_tunnels)
+    logger.info("  Maximum number of tunnels: %d" % self.max_tunnels)
     logger.info("  Interface: %s" % self.interface)
     logger.info("  Address: %s" % self.address)
     logger.info("  Ports: %s" % self.ports)
@@ -832,7 +832,7 @@ class TunnelManager(object):
     self.restore_netfilter()
     # Close any stale tunnels that might still be up
     id_base = config.getint('broker', 'tunnel_id_base')
-    self.tunnel_ids = range(id_base, id_base + max_tunnels + 1)
+    self.tunnel_ids = range(id_base, id_base + self.max_tunnels + 1)
     self.setup_tunnels()
   
   def issue_cookie(self, endpoint):
