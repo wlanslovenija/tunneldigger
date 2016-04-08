5e06c28510798db3b1a4927f13844fd9cf44621d
Alexander Couzens <lynxis@fe80.eu>
2016-04-08 10:44:07 +0200

    PREPARE: Correct length and type of the tunnel id
    the tunnel id is an unsigned 32bit integer

--- broker/protocol.py
+++ broker/protocol.py
@@ -118,7 +118,7 @@ class HandshakeProtocolMixin(object):
             uuid_len = struct.unpack('!B', msg_data[8])[0]
             uuid = msg_data[9:9 + uuid_len]
             try:
-                remote_tunnel_id = struct.unpack('!H', msg_data[10 + uuid_len:10 + uuid_len + 2])[0]
+                remote_tunnel_id = struct.unpack('!I', msg_data[10 + uuid_len:10 + uuid_len + 4])[0]
             except struct.error:
                 remote_tunnel_id = 1
 
