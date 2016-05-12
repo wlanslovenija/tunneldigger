84c922d3c425cb2594e44ee484c25a78637265d6
Alexander Couzens <lynxis@fe80.eu>
2016-05-13 01:35:38 +0200

    tests: move tests into a library and improve failure case

--- tests/jenkins.sh
+++ tests/jenkins.sh
@@ -5,40 +5,28 @@
 
 set -e
 
-# compile test the l2tp client
-echo "Try to compile the l2tp client"
-cd $WORKSPACE/client/
-make
-
-# prepare lxc container template
-$WORKSPACE/tests/tunneldigger.py --setup
+. $(dirname $0)/lib_ci.sh
 
 # retrieve git rev
-cd $WORKSPACE/
-NEW_REV=$(git log -1 --format=format:%H)
+NEW_REV=$(cd $WORKSPACE && git log -1 --format=format:%H)
 
-cd $WORKSPACE/tests/
+# compile the client
+test_client_compile
+
+# setup the base container
+setup_container
+
 # test the version aginst itself
-export CLIENT_REV=$NEW_REV
-export SERVER_REV=$NEW_REV
-nosetests3 test_nose.py
-
+test_nose $NEW_REV $NEW_REV
 
 OLD_REV="c638231efca6b3a6e1c675ac0834a3e851ad1bdc 4e4f13cdc630c46909d47441093a5bdaffa0d67f"
-# do client NEW_REV against old revs
+# test against each other
 for rev in $OLD_REV ; do
-  export CLIENT_REV=$NEW_REV
-  export SERVER_REV=$rev
-  nosetests3 test_nose.py
+  # old client, new server
+  test_nose $rev $NEW_REV
+
+  # new client, old server
+  test_nose $NEW_REV $rev
 done
 
-# do server NEW_REV against old revs
-for rev in $OLD_REV ; do
-  export CLIENT_REV=$rev
-  export SERVER_REV=$NEW_REV
-  nosetests3 test_nose.py
-done
-
-for i in seq 1 5; do
-  nosetests3 test_usage.py
-done
+test_usage $NEW_REV
