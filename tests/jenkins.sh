488f5e165baac67bb598ebe90cb71c60874826dc
Alexander Couzens <lynxis@fe80.eu>
2016-05-13 00:19:07 +0200

    tests: remove nosetests function
    reduce also the time by 1/3 because when executing the usage test,
    they don't execute the default tests.

--- tests/jenkins.sh
+++ tests/jenkins.sh
@@ -3,10 +3,6 @@
 #
 # jenkins script
 
-nosetests() {
-  nosetests3 test_nose.py
-}
-
 set -e
 
 # compile test the l2tp client
@@ -25,7 +21,7 @@ cd $WORKSPACE/tests/
 # test the version aginst itself
 export CLIENT_REV=$NEW_REV
 export SERVER_REV=$NEW_REV
-nosetests
+nosetests3 test_nose.py
 
 
 OLD_REV="c638231efca6b3a6e1c675ac0834a3e851ad1bdc 4e4f13cdc630c46909d47441093a5bdaffa0d67f"
@@ -33,16 +29,16 @@ OLD_REV="c638231efca6b3a6e1c675ac0834a3e851ad1bdc 4e4f13cdc630c46909d47441093a5b
 for rev in $OLD_REV ; do
   export CLIENT_REV=$NEW_REV
   export SERVER_REV=$rev
-  nosetests
+  nosetests3 test_nose.py
 done
 
 # do server NEW_REV against old revs
 for rev in $OLD_REV ; do
   export CLIENT_REV=$rev
   export SERVER_REV=$NEW_REV
-  nosetests
+  nosetests3 test_nose.py
 done
 
 for i in seq 1 5; do
-  nosetests test_usage.py
+  nosetests3 test_usage.py
 done
