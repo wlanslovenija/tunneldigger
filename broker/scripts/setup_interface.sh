56e8d35af24d13c6c707b2991a02cdbfb9294755
Ralf Jung <post@ralfj.de>
2019-01-08 20:34:37 +0100

    pass the local broker port to the session setup scripts

--- broker/scripts/setup_interface.sh
+++ broker/scripts/setup_interface.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 TUNNEL_ID="$1"
+SESSION_ID="$2"
 INTERFACE="$3"
 MTU="$4"
+ENDPOINT_IP="$5"
+ENDPOINT_PORT="$6"
+LOCAL_PORT="$7"
+UUID="$8"
+LOCAL_BROKER_PORT="$9"
 
 . scripts/bridge_functions.sh
 
