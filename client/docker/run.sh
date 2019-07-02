432991181436490678a839dba7fa0484273de81b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-07-02 12:35:39 +0200

    check for privileged mode

--- client/docker/run.sh
+++ client/docker/run.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+#
+# Check mode, see
+# https://github.com/hwdsl2/docker-ipsec-vpn-server/blob/f254eacd6e081020939b568541c7c3a50663d475/run.sh#L37
+#
+if ip link add dummy0 type dummy 2>&1 | grep -q "not permitted"; then
+  cat 1>&2 <<'  EOF'
+  Error: This Docker image must be run in privileged mode.
+  For detailed instructions, please visit:
+  https://tunneldigger.readthedocs.io/en/latest/server.html
+  EOF
+  exit 1
+fi
+ip link delete dummy0 >/dev/null 2>&1
+
+#
+# Set default values
+#
 UUID="`cat /dev/random | head -c 5 | base64 | grep -oE '[^=]+'`"
 IFACE="l2tp0"
 HOOK="/srv/tunneldigger/client/hook.sh"
 
+#
+# Parse parameters and replace default values
+#
 args=()
 i=0
 
@@ -35,6 +55,9 @@ while [ -n "$1" ]; do
   fi
 done
 
+#
+# Run tunneldigger
+#
 echo /usr/local/bin/tunneldigger -f -i "$IFACE" -s "$HOOK" -u "$UUID" "${args[@]}"
 /usr/local/bin/tunneldigger -f -i "$IFACE" -s "$HOOK" -u "$UUID" "${args[@]}"
 
