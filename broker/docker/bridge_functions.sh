c2fd9381ff45589884c9a62592abd43ef92f2536
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-07-07 17:06:10 +0200

    restart dhcp server only when interfaces are added

--- broker/docker/bridge_functions.sh
+++ broker/docker/bridge_functions.sh
@@ -5,18 +5,20 @@ add_to_dhcp_server()
   # see https://askubuntu.com/a/184351/136346
   (
     source /etc/default/isc-dhcp-server
-    INTERFACESv4=`echo $INTERFACESv4 $@ | tr "[:space:]" "\n" | uniq`
-    echo INTERFACESv4=\"$INTERFACESv4\" > /etc/default/isc-dhcp-server
-  )
+    newINTERFACESv4=`echo $INTERFACESv4 $@ | tr "[:space:]" "\n" | uniq | sort`
+    if [ "$newINTERFACESv4" != "$INTERFACESv4" ]; then
+      echo INTERFACESv4=\"$newINTERFACESv4\" > /etc/default/isc-dhcp-server
 
-  if ! service isc-dhcp-server restart; then
-    for file in /etc/default/isc-dhcp-server /etc/dhcp/dhcpd.conf; do
-      echo --------------- $file --------------- 
-      cat $file
-    done
-    echo ------------------------------------
-    /usr/sbin/dhcpd -f /etc/dhcp/dhcpd.conf
-    exit 1
-  fi
+      if ! service isc-dhcp-server restart; then
+        for file in /etc/default/isc-dhcp-server /etc/dhcp/dhcpd.conf; do
+          echo --------------- $file --------------- 
+          cat $file
+        done
+        echo ------------------------------------
+        /usr/sbin/dhcpd -f /etc/dhcp/dhcpd.conf
+        exit 1
+      fi
+    fi
+  )
 }
 
