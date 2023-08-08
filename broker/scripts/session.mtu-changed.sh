ccff44d7f67042310bd32ec5a0e4bffaa188dbc9
Felix Kaechele <felix@kaechele.ca>
2023-08-07 23:38:23 -0400

    ensure bridge port isolation is on before setting link up
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- broker/scripts/session.mtu-changed.sh
+++ broker/scripts/session.mtu-changed.sh
@@ -11,4 +11,10 @@ ip link set dev $INTERFACE nomaster
 
 # Change interface MTU and add to new bridge
 ensure_bridge digger${NEW_MTU}
-ip link set dev $INTERFACE master digger${NEW_MTU} mtu $NEW_MTU up
+ip link set dev $INTERFACE master digger${NEW_MTU} mtu $NEW_MTU
+
+# Turn on bridge port isolation
+bridge link set dev $INTERFACE isolated on
+
+# Bring the tunnel interface up only after port isolation is enabled
+ip link set dev $INTERFACE up
--- broker/scripts/session.up.sh
+++ broker/scripts/session.up.sh
@@ -13,7 +13,10 @@ LOCAL_BROKER_PORT="$9"
 
 # Add the interface to our bridge
 ensure_bridge digger${MTU}
-ip link set dev $INTERFACE master digger${MTU} mtu $MTU up
+ip link set dev $INTERFACE master digger${MTU} mtu $MTU
 
 # Turn on bridge port isolation
 bridge link set dev $INTERFACE isolated on
+
+# Bring the tunnel interface up only after port isolation is enabled
+ip link set dev $INTERFACE up
