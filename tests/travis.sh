ffe375c423b2d3239c1000c7efdd0819036ef5a4
Ralf Jung <post@ralfj.de>
2023-08-15 20:15:39 +0200

    add GHA CI

--- tests/travis.sh
+++ tests/travis.sh
@@ -5,25 +5,23 @@
 
 . $(dirname $0)/lib_ci.sh
 
-set -e
-
 export WORKSPACE=$PWD
-export PATH=/usr/bin/:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin
-unset VIRTUAL_ENV
+#export PATH=/usr/bin/:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin
+#unset VIRTUAL_ENV
 
-# retrieve git rev
-NEW_REV=$TRAVIS_COMMIT
-
-# OLD_REV defined by .travis.yml
+# NEW_REV, OLD_REV defined by .travis.yml
 # SELECT too
 
 if [ "$OLD_REV" = "HEAD" ] ; then
-  OLD_REV=$TRAVIS_COMMIT
+  OLD_REV=$NEW_REV
 fi
 
 # We had a lot of intermittent failures with the default keyserver (pool.sks-keyservers.net).
 #export DOWNLOAD_KEYSERVER="pgp.mit.edu"
 #export DOWNLOAD_KEYSERVER="keyserver.ubuntu.com"
+
+set -ex
+/usr/bin/env python --version
 
 # finish setup
 $(dirname $0)/tunneldigger.py --setup bionic
