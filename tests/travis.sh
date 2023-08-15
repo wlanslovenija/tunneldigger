f33079e401b47a7644f6e3a673efcb9366cb6a64
Ralf Jung <post@ralfj.de>
2023-08-15 21:48:11 +0200

    ubuntu trusty is not on LXC any more
    I can't get the really old versions to run on Bionic. I hope this means nobody uses them any more.

--- tests/travis.sh
+++ tests/travis.sh
@@ -5,37 +5,25 @@
 
 . $(dirname $0)/lib_ci.sh
 
-export WORKSPACE=$PWD
 #export PATH=/usr/bin/:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin
 #unset VIRTUAL_ENV
 
 # NEW_REV, OLD_REV defined by .travis.yml
 # SELECT too
 
-if [ "$OLD_REV" = "HEAD" ] ; then
-  OLD_REV=$NEW_REV
-fi
-
 # We had a lot of intermittent failures with the default keyserver (pool.sks-keyservers.net).
 #export DOWNLOAD_KEYSERVER="pgp.mit.edu"
 #export DOWNLOAD_KEYSERVER="keyserver.ubuntu.com"
-
-set -ex
-/usr/bin/env python --version
-
-# finish setup
-$(dirname $0)/tunneldigger.py --setup bionic
-if [[ "$OLD_UBUNTU" == "trusty" || "$SELECT" == "usage" ]]; then
-    $(dirname $0)/tunneldigger.py --setup trusty
-fi
 
 # run required test
 
 case "$SELECT" in
   nose)
-    test_nose $OLD_REV $OLD_UBUNTU $NEW_REV
+    setup_container
+    test_nose $OLD_REV $NEW_REV
     ;;
   usage)
+    setup_container
     test_usage $NEW_REV
     ;;
   client)
